a88b44df239e034bea89f1e79319610ff363a825
Mikołaj Krzyszczak <miwo.krzyszczak@gmail.com>
2018-05-03 11:03:16 +0200

    Adding SpringBoot

---
+++ allegroMovies/src/main/java/com/krzyszczak/allegroMovies/AllegroMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.krzyszczak.allegroMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegroMoviesApplication.class, args);
+	}
+}
